1e10ea002b9e32ede79881b874f2c6af7c63f94d
AhmedReda178 <rda49586@gmail.com>
2026-05-10 01:41:12 +0300

    class Badge

---
+++ EventHub/Models/Badge.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventHub.Models
+{
+    public enum BadgeTier { Standard, VIP }
+
+    public class Badge
+    {
+        public int Id { get; set; }
+        public int BadgeNumber { get; set; }
+        public DateTime IssueDate { get; set; }
+        public BadgeTier Tier { get; set; }
+
+        public int AttendeeId { get; set; }
+        public Attendee Attendee { get; set; }
+    }
+}
