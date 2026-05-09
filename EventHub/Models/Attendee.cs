1cd1184b9fdd3910da8b4da07fac8bcea1bf87aa
AhmedReda178 <rda49586@gmail.com>
2026-05-10 01:39:48 +0300

    class Attendee

---
+++ EventHub/Models/Attendee.cs
@@ -0,0 +1,39 @@
+﻿using Azure;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventHub.Models
+{
+    public class Attendee
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string FullName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string Street { get; set; }
+
+        [Required]
+        public string City { get; set; }
+
+        [Required]
+        public string Country { get; set; }
+
+        [Required]
+        public string PostalCode { get; set; }
+
+        public Badge? Badge { get; set; }
+        public ICollection<Registration> Registrations { get; set; } = new List<Registration>();
+    }
+}
